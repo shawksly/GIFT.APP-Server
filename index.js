4669543a78b1f07daa5cbe6ca437c42d94428cfa
J-Krueger-Dev <Jkrueger212@gmail.com>
2023-11-18 18:24:39 -0700

    inital buildout for server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // GIFT APP II
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose')
+
+const{PORT,MONGO} = process.env;
+
+mongoose.connect(`${process.env.MONGO}/GiftMe`)
+
+const db = mongoose.connection;
+
+db.once('open', ()=>console.log(`connected to: ${MONGO}`));
+
+app.use(express.json());
+
+//! CONTROLLERS TO BE PLACED HERE
+
+
+
+//! App.use placed here
+
+app.get('/test', (req,res)=> {
+  res.status(200).json({message: `server is accessible`, port: process.env.PORT})
+})
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
