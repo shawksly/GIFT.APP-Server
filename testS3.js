f74d6f1716a436b4cd5ad0d4a99510cd2699e085
J-Krueger-Dev <Jkrueger212@gmail.com>
2023-12-02 16:11:51 -0700

    corrections...again

--- testS3.js
+++ testS3.js
@@ -6,15 +6,15 @@ imgForm.addEventListenter('submit', async (e) =>{
   const file = e.target[0].files[0];
   console.log(file);
 
-  // fetch to server endpoint to get the link (from s3)
+  //! fetch to server endpoint to get the link (from s3)
   const url = await fetch("/geturl")
   .then((res)=>res.json())
   .then((data)=> console.log(data))
-  // fetch to s3 to upload the image (PUT)
-  await FileSystemDirectoryHandle(url,{
+  //! fetch to s3 to upload the image (PUT)
+  await fetch(url,{
     method: "PUT",
     headers: {
-      'Content-Type': "mulitpart/form-data"
+      'Content-Type': "multipart/form-data"
     },
     body: file,
   });
@@ -24,7 +24,7 @@ imgForm.addEventListenter('submit', async (e) =>{
   const img = document.createElement("img")
   img.src = imgURL
   document.body.appendChild(img)
-  // fetch to our server's db to post the link
+  //! fetch to our server's db to post the link
   await fetch("/create", {
     method: "POST",
     headers: {
